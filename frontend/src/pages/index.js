79f20a8150dbbcf6e994fe92e5b2936195370180
Ezra Singh <ezra@liontech.nyc>
2020-03-03 08:27:41 -0500

    Updated boilerplate frontend

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,12 +1,41 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import { Page, SEO } from "../components/ui"
+import { Page, Section, SEO } from "../components/ui"
 
 const IndexPage = () => (
-  <Page>
+  <Page
+    pageLayout="vertical"
+    contentLayout="vertical"
+    contentSpacing="wide"
+    stickyNav
+    justifyNavContent="end"
+  >
     <SEO title="Home" />
-    <section className="page__content--block">
+    <Section>
+      <h1>Hi people, welcome to Inergy CMS Frontend</h1>
+      <Section>
+        <em>Please note that is a boilerplate and you should necessarily remove the books content type!</em>
+        <br/><br/>
+        Here is a simple list of <i>books</i> from the backend:
+        <br/>
+        <br/>
+        <Link to="/modal-example/" state={{ modal: true }}>
+          Open modal
+        </Link>
+      </Section>
+      <Section>
+        <em>Please note that is a boilerplate and you should necessarily remove the books content type!</em>
+        <br/><br/>
+        Here is a simple list of <i>books</i> from the backend:
+        <br/>
+        <br/>
+        <Link to="/modal-example/" state={{ modal: true }}>
+          Open modal
+        </Link>
+      </Section>
+    </Section>
+    <Section>
       <h1>Hi people, welcome to Inergy CMS Frontend</h1>
       <span>
         <em>Please note that is a boilerplate and you should necessarily remove the books content type!</em>
@@ -18,7 +47,7 @@ const IndexPage = () => (
           Open modal
         </Link>
       </span>
-    </section>
+    </Section>
   </Page>
 )
 
