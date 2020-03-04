d04f2cc3d6719a1b6e376d006458eccaf9130c8c
Ezra Singh <ezra@liontech.nyc>
2020-03-04 14:57:59 -0500

    Wired `id` prop to <section> wrapper

--- frontend/src/components/ui/section.js
+++ frontend/src/components/ui/section.js
@@ -2,14 +2,14 @@ import React from "react"
 import PropTypes from "prop-types"
 import cx from "classnames"
 
-const Section = ({ isNarrow, contentLayout, style, children }) => {
+const Section = ({ isNarrow, contentLayout, id, style, children }) => {
     const contentBlockClass = cx("page__content--block", { 
         "is-narrow" : isNarrow,
         "is-horizontal" : contentLayout === 'horizontal',
         "is-vertical" : contentLayout === 'vertical'
     })
     return(
-        <section className={contentBlockClass} style={style}>
+        <section id={id} className={contentBlockClass} style={style}>
             { children }
         </section>
     )
@@ -18,12 +18,14 @@ const Section = ({ isNarrow, contentLayout, style, children }) => {
 Section.propTypes = {
     isNarrow: PropTypes.bool,
     contentLayout: PropTypes.string,
+    id: PropTypes.string,
     style: PropTypes.object,
     children: PropTypes.node.isRequired
 }
 
 Section.defaultProps = {
     contentLayout: 'vertical',
+    id: undefined,
     style: undefined
 }
 
