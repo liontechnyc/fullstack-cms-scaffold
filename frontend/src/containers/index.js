4ca878fb52ac3d1899138488142fa514257d6935
Ezra Singh <ezra@liontech.nyc>
2020-03-05 22:44:05 -0500

    Cleaned up code base

--- frontend/src/containers/index.js
+++ frontend/src/containers/index.js
@@ -2,3 +2,4 @@
 export { default as Services } from "./services"
 export { default as Solutions } from "./solutions"
 export { default as Pricing } from "./pricing"
+export { default as ContactInfo } from "./contact-info"
