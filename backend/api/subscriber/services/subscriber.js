a4371a5a73b083bd6e12a4358ae724bab5c4a754
Ezra Singh <ezra@liontech.nyc>
2020-03-12 21:17:49 -0400

    Set strapi config interface in mailchimp service

--- backend/api/subscriber/services/subscriber.js
+++ backend/api/subscriber/services/subscriber.js
@@ -2,19 +2,21 @@
 const crypto = require('crypto');
 const Mailchimp = require('mailchimp-api-v3');
 
+const ENV = strapi.config.environment
+
 function md5(payload){
     return crypto.createHash('md5').update(payload).digest('hex');
 };
 
 module.exports = {
     addNewSubscriber: (email) => {
-        const mailchimp = new Mailchimp(strapi.config.mailchimpApiKey);
+        const mailchimp = new Mailchimp(strapi.config.environments[ ENV ].mailchimpApiKey);
         const hash = md5(String(email).toLowerCase())
         return mailchimp.request({
             method: 'put',
             path: '/lists/{list_id}/members/{subscriber_hash}',
             path_params: {
-                list_id: strapi.config.mailchimpAudience,
+                list_id: strapi.config.environments[ ENV ].mailchimpAudience,
                 subscriber_hash: hash
             },
             body: {
@@ -30,7 +32,7 @@ module.exports = {
             method: 'delete',
             path: '/lists/{list_id}/members/{subscriber_hash}',
             path_params: {
-                list_id: strapi.config.mailchimpAudience,
+                list_id: strapi.config.environments[ ENV ].mailchimpAudience,
                 subscriber_hash: hash
             }
         })
